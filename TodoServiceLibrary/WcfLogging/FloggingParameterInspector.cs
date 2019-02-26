a4a56148424b59b633decc5d9835fb8f953a2781
Joe Mumm <jmummdbd@att.net>
2019-02-25 21:32:49 -0800

    Added GetDetails helper method to pull and flatten ToDoItem in FloggingParameterInspector class.  Behavior extension shows Warning in TodoServiceLibrary app.config but code works.

--- TodoServiceLibrary/WcfLogging/FloggingParameterInspector.cs
+++ TodoServiceLibrary/WcfLogging/FloggingParameterInspector.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel.Dispatcher;
+using System.Text;
 
 namespace TodoServiceLibrary.WcfLogging
 {
@@ -22,10 +23,20 @@ namespace TodoServiceLibrary.WcfLogging
       var details = new Dictionary<string, object>();
       if (inputs != null)
         for (int i = 0; i < inputs.Count(); i++)
-          details.Add($"input-{i}", inputs[i] != null ? inputs[i].ToString() : "");
+          details.Add($"input-{i}:", inputs[i] != null ? GetDetails(inputs[i]) : "");
 
       return WcfLogger.GetWcfLogEntry(operationName, details,
           serviceName: _serviceName);
+    }
+
+    private string GetDetails(object input)
+    {
+      var props = input.GetType().GetProperties();
+      var sb = new StringBuilder();
+      foreach (var prop in props)
+        sb.Append($"{prop.Name}:{prop.GetValue(input).ToString()} -- ");
+
+      return sb.ToString().Substring(0, sb.Length-4);
     }
 
     public void AfterCall(string operationName, object[] outputs,
